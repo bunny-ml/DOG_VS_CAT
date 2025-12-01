73899b3d76dff99a3ac7a1d7235fdbcb4b0165dd
bunny-ml <abhishek260204@gmail.com>
2025-12-02 00:07:25 +0530

    commit

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+
+# Load model
+model = tf.keras.models.load_model("best_cat_dog_model.keras")
+
+st.title("🐱🐶 Cat vs Dog Classifier")
+
+uploaded_file = st.file_uploader("Upload an image...", type=["jpg", "jpeg", "png"])
+
+def preprocess(img):
+    img = img.resize((256, 256))
+    img = np.array(img) / 255.0
+    img = np.expand_dims(img, axis=0)
+    return img
+
+if uploaded_file is not None:
+    img = Image.open(uploaded_file).convert("RGB")
+    st.image(img, caption="Uploaded Image")
+
+    img_array = preprocess(img)
+
+    pred = model.predict(img_array)[0][0]
+
+    if pred >= 0.5:
+        st.success("Prediction: **DOG 🐶**")
+    else:
+        st.success("Prediction: **CAT 🐱**")
